d07981a3cb3c1d34f0512f539e591414304ef8e3
Jonathan Pereira <jonathan14willian@gmail.com>
2023-08-14 19:53:25 -0300

    Init

---
+++ main.py
@@ -0,0 +1,13 @@
+# Métrica: Quantidade de estudantes
+# Autor: Jonathan Pereira
+# Data: 2023-08-14
+# Versão: 0.0.1
+
+# Dimensoes 
+# 1 - UF
+# 2 - Município
+# 3 - Instituição
+# 4 - Modalidade de ensino
+# 5 - Curso
+
+# Criar dimensões e tabela fato
