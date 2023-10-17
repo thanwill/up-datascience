ccd7a8e975c118f27bc83ca1bc2977251feeab70
Jonathan <jonathan14willian@gmail.com>
2023-10-16 21:54:58 -0300

    ExponentialSmoothing

--- documento.py
+++ documento.py
@@ -12,16 +12,17 @@ from statsmodels.tsa.seasonal import seasonal_decompose # importa o método de d
 # root = Tk()
 # root.filename = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
 filename = 'C:/Users/Aluno/Downloads/airline-passengers.csv' # caminho do arquivo
-df = pd.read_csv(root.filename, sep=',', parse_dates=True, index_col='Month' ) # lê o arquivo csv e define a coluna Month como índice
-f = open('path.txt', 'w') # abre o arquivo path.txt em modo de escrita
-f.write(root.filename) # escreve o caminho do arquivo no arquivo path.txt
-f.close() # fecha o arquivo path.txt
+df = pd.read_csv(filename, sep=',', parse_dates=True, index_col='Month' ) # lê o arquivo csv e define a coluna Month como índice
+#f = open('path.txt', 'w') # abre o arquivo path.txt em modo de escrita
+#f.write(root.filename) # escreve o caminho do arquivo no arquivo path.txt
+#f.close() # fecha o arquivo path.txt
 
 #### CONFIGURAÇÃO ####
  
 result = seasonal_decompose(df['Passengers'], model='multiplicative') # cria a decomposição sazonal multiplicativa
 # result = seasonal_decompose(df['Passengers'], model='additional') # cria a decomposição sazonal aditiva
-result.plot() 
+# result.plot() 
+# df.plot() # cria o gráfico
 
 # Definir a frequencia  do ínidce como mensal
 df.index.freq = 'MS' # MS: mensal, AS: anual, W: semanal, D: diário
@@ -34,7 +35,20 @@ from statsmodels.tsa.holtwinters import SimpleExpSmoothing
 model = SimpleExpSmoothing(df['Passengers']) # cria o modelo
 fitted_model = model.fit(smoothing_level=alpha, optimized=False, use_brute=True) # ajusta o modelo fit( parametro de suavização, otimização, uso de força bruta)
 df['SES12'] = fitted_model.fittedvalues.shift(-1) # cria a coluna SES12 e atribui os valores ajustados pelo modelo
-df.plot() # cria o gráfico
+# df.plot() # cria o gráfico
+
+# exponencial duplo aditivo e multiplicativo
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+df['HWES2_ADD'] = ExponentialSmoothing(df['Passengers'], trend='add').fit().fittedvalues
+df['HWES2_MUL'] = ExponentialSmoothing(df['Passengers'], trend='mul').fit().fittedvalues
+df['e'] = df['Passengers'] - df['HWES2_ADD'] # cria a coluna e e atribui a diferença entre os valores reais e os valores ajustados pelo modelo
+df[['Passengers', 'HWES2_ADD', 'HWES2_MUL']].plot(title='Holt Winters Double Exponential Smoothing: ') # cria o gráfico
+# cria o grafico com o valor real, o valor ajustado pelo modelo e o erro
+df[['Passengers', 'HWES2_ADD', 'e']].plot(subplots=True, figsize=(15,8)) # cria o gráfico
+
+
 plt.show() # mostra o gráfico
+
 
 
