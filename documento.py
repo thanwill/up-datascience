7791015e6f855ecb4284ac50fb4c007f9a467d0d
Jonathan <jonathan14willian@gmail.com>
2023-10-16 21:38:09 -0300

    Simple Exponential Smoothing

--- documento.py
+++ documento.py
@@ -1,31 +1,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 from statsmodels.tsa.seasonal import seasonal_decompose # importa o método de decomposição sazonal pip install statsmodels
 
 
-root = Tk()
+#### CONFIGURAÇÃO ####
 
+# root = Tk()
+# root.filename = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+filename = 'C:/Users/Aluno/Downloads/airline-passengers.csv' # caminho do arquivo
+df = pd.read_csv(root.filename, sep=',', parse_dates=True, index_col='Month' ) # lê o arquivo csv e define a coluna Month como índice
+f = open('path.txt', 'w') # abre o arquivo path.txt em modo de escrita
+f.write(root.filename) # escreve o caminho do arquivo no arquivo path.txt
+f.close() # fecha o arquivo path.txt
 
-root.filename = 'C:/Users/Aluno/Downloads/airline-passengers.csv'
-# filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
-df = pd.read_csv(root.filename, sep=',', parse_dates=True, index_col='Month' )
-f = open('path.txt', 'w')
-f.write(root.filename)
-f.close()
+#### CONFIGURAÇÃO ####
+ 
+result = seasonal_decompose(df['Passengers'], model='multiplicative') # cria a decomposição sazonal multiplicativa
+# result = seasonal_decompose(df['Passengers'], model='additional') # cria a decomposição sazonal aditiva
+result.plot() 
 
-# cria um gráfico de linhas
-df.plot()
-plt.show()
+# Definir a frequencia  do ínidce como mensal
+df.index.freq = 'MS' # MS: mensal, AS: anual, W: semanal, D: diário
+meses = 12
+alpha = 1/( 2 * meses) # suavização simples: formula do alpha alpha = 1/(2*m) aonde m é o número de períodos
 
+# Aplicar o método de Holt-Winters: Simple Exponential Smoothing
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
 
-# trend 
-result = seasonal_decompose(df['Passengers'], model='multiplicative')
-result.plot()
-plt.show()
+model = SimpleExpSmoothing(df['Passengers']) # cria o modelo
+fitted_model = model.fit(smoothing_level=alpha, optimized=False, use_brute=True) # ajusta o modelo fit( parametro de suavização, otimização, uso de força bruta)
+df['SES12'] = fitted_model.fittedvalues.shift(-1) # cria a coluna SES12 e atribui os valores ajustados pelo modelo
+df.plot() # cria o gráfico
+plt.show() # mostra o gráfico
 
 
