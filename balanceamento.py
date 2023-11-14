22865b43d2bd62caf7531f15b619ad9147b3d338
Jonathan <jonathan14willian@gmail.com>
2023-11-13 21:46:30 -0300

    Add decision tree classifier and confusion matrix display

--- balanceamento.py
+++ balanceamento.py
@@ -26,6 +26,28 @@ dados_atributos = fertility.drop('Output', axis=1)
 # divisão dos dados em treino e teste (70% e 30%) respectivamente
 dados_atributos_treino, dados_atributos_teste, dados_classes_treino, dados_classes_teste = train_test_split(dados_atributos, dados_classes, test_size=0.3, random_state=1)
 
+# print(dados_classes_teste)
+
+tree = DecisionTreeClassifier()
+fertility_balanceado = tree.fit(dados_atributos_treino, dados_classes_treino)
+
+classe_predita = fertility_balanceado.predict(dados_atributos_teste)
+
+# print(classe_predita)
+
+# ---------------------------------------------------
+
+# Matriz de contingência / Confusion Matrix (TP, FP, FN, TN)
+
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+matriz_confusao = confusion_matrix(dados_classes_teste, classe_predita)
+disp = ConfusionMatrixDisplay(confusion_matrix=matriz_confusao, display_labels=fertility_balanceado.classes_)
+disp.plot()
+plt.show()
+
 
 
 
